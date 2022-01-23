80c9635d2e714041f0486fc98ec4c91f08c48a8d
James Ray <rihoj1@gmail.com>
2022-01-22 22:41:50 -0500

    frontend -  move s3 tf to it's own file

--- infrastructure/frontend/main.tf
+++ infrastructure/frontend/main.tf
@@ -17,38 +17,6 @@ provider "aws" {
   alias  = "east-1"
 }
 
-#tfsec:ignore:aws-s3-enable-bucket-logging
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.name}.${var.domain}"
-  policy = data.template_file.init.rendered
-  acl    = "private"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "arn"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "bucket_pab" {
-  bucket                  = aws_s3_bucket.bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_object" "index" {
-  bucket = "${var.name}.${var.domain}"
-  key    = "index.html"
-  source = "${path.module}/../../frontend/index.html"
-}
-
 #tfsec:ignore:aws-cloudfront-enable-logging
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
@@ -100,13 +68,4 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   depends_on = [aws_acm_certificate_validation.ssl]
 
   web_acl_id = aws_wafv2_web_acl.waf_acl.id
-}
-
-data "template_file" "init" {
-  template = file("${path.module}/policy.tpl")
-  vars = {
-    name       = var.name
-    domain     = var.domain
-    cloudfront = aws_cloudfront_distribution.s3_distribution.id
-  }
 }
