a3e96b254f7a02159f6e08ac656723c1ebe553fe
James Ray <rihoj1@gmail.com>
2022-01-22 21:58:56 -0500

    frontend -  fix no-public-buckets

--- infrastructure/frontend/main.tf
+++ infrastructure/frontend/main.tf
@@ -35,12 +35,12 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket              = aws_s3_bucket.bucket.id
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = false
-
+resource "aws_s3_bucket_public_access_block" "bucket_pab" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_object" "index" {
