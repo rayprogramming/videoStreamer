306e769ed7838f6315aaefe984496a833edb1878
James Ray <rihoj1@gmail.com>
2022-04-16 23:07:20 -0400

    remove amplify because I'm tired of it

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,4 @@
 <template>
-  <amplify-authenticator username-alias="email">
-     <img alt="Vue logo" src="../assets/logo.png">
-     <HelloWorld msg="You Successfully Authenticated a User!  Go You!"/>
-     <amplify-sign-out></amplify-sign-out>
- </amplify-authenticator>
 </template>
 
 <script>
