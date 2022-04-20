2f208ebe8915fa55fd19edf2d7b681fee3bcc221
James Ray <rihoj1@gmail.com>
2022-04-19 22:19:46 -0400

    fix iam issues

--- infrastructure/backend/users/main.tf
+++ infrastructure/backend/users/main.tf
@@ -25,3 +25,15 @@ locals {
 data "aws_route53_zone" "selected" {
   zone_id = var.zoneid
 }
+
+
+data "terraform_remote_state" "iam" {
+  backend = "s3"
+
+  config = {
+    bucket     = "rayprogramming-terraform"
+    key        = "iam"
+    kms_key_id = "alias/TwoRivers-sym"
+    region     = "us-east-2"
+  }
+}
